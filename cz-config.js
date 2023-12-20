e1ded4615da8d1fe86c52594c01052718c71f2cb
Moka <romeramatias@gmail.com>
2023-12-20 15:12:25 -0300

    prueba

---
+++ cz-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    types: [
+        { value: "feat", name: "feat:     A new feature" },
+        { value: "fix", name: "fix:      A bug fix" },
+        // ... otros tipos según tus necesidades
+    ],
+    // ... otras configuraciones según tus necesidades
+};
